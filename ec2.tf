e0151e0dbe42902e37fef21ba8ba2e2d47fc597b
“Manzi” <manziwilly@aol.com>
2022-11-09 05:49:43 -0500

    last one

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
-  nstance_type = var.instance_type
-  metadata_options {
-  http_endpoint = "disbale"
-  http_tokens   = "required"
-}
+  instance_type = var.instance_type
 
-}
   monitoring = true
 
   ebs_optimized = true
@@ -18,3 +13,4 @@ root_block_device {
   tags = {
     Name = "HelloWorld"
   }
+}
